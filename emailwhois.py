ae4fd7f084fcd90933f151121f3a8f7b0a5488b1
WebBreacher <webbreacher@gmail.com>
2016-06-13 07:42:51 -0400

    Added argsparse for cmdline arg passing, try/except error handling, if/then for NOTFOUND doms.

--- emailwhois.py
+++ emailwhois.py
@@ -10,12 +10,14 @@
 '''
 
 # Tell python we want to use a library
-import csv # https://docs.python.org/2/library/csv.html
+import argparse
+import csv
 from datetime import date
 import pprint
-import re
-import urllib2
 import pythonwhois # http://cryto.net/pythonwhois/index.html
+import re
+import sys
+import urllib2
 
 ####
 # Variables
@@ -23,18 +25,23 @@ import pythonwhois # http://cryto.net/pythonwhois/index.html
 # Pretend to be a normal web browser instead of "Python"
 # Large list of User Agents are http://techpatterns.com/downloads/firefox/useragentswitcher.xml
 user_agent = 'Mozilla/5.0 (compatible; Googlebot/2.1;  http://www.google.com/bot.html)'
-targetDomain = 'dhs.gov'
 csv_output = {}
 pp = pprint.PrettyPrinter(indent=4)
+
+# Parse command line input
+parser = argparse.ArgumentParser(description="To look up an email wildcard and find all domains reg'd with it")
+parser.add_argument('-d', '--domain', required=True, help='Single domain to search for (Ex: dhs.gov)')
+args = parser.parse_args()
+
 
 ####
 # Setting up and Making the Web Call
 ####
 # Create and make the call
-url = 'http://viewdns.info/reversewhois/?q=%40' + targetDomain
+url = 'http://viewdns.info/reversewhois/?q=%40' + args.domain
 req = urllib2.Request(url)
 req.add_header('User-Agent', user_agent)
-response = urllib2.urlopen(req, timeout=20)
+response = urllib2.urlopen(req, timeout=15)
 resp_data = response.read()
 
 
@@ -49,18 +56,28 @@ for line in data:
     line = re.sub('</td>', '', line)
     domains = line.split('<td>')
 
-    # Make a whois lookup of the domain and pull out the email@bah.com
-    w = pythonwhois.get_whois(domains[1], normalized=True)
-    
-    print '------------------------------------------------------------'
-    print "%s, %s, %s" % (domains[1],domains[2],domains[3])
-    print ',\n'.join(['   %s: %s' % (key, value) for (key, value) in w['contacts']['admin'].items()]) 
+    # Make a whois lookup of the domain and pull out the email@dhs.gov
+    try:
+        w = pythonwhois.get_whois(domains[1], normalized=True)
 
-    #print w['contacts']
-    
-    #pp.pprint(w)
-    #csv_output[domains[1]] = w
-    #print csv_output[domains[1]]
+        print '------------------------------------------------------------'
+        print '%s, %s, %s' % (domains[1],domains[2],domains[3])
+        for response in w['raw']:
+            if re.match('NOT FOUND', response):
+                print '[!] ERRROR with %s' % domains[1]
+                continue
+            else:
+                print '\n'.join(['   %s: %s' % (key, value) for (key, value) in w['contacts']['admin'].items()]) 
+                print '     ------'
+                #print w['contacts']
+                
+                #pp.pprint(w)
+                #csv_output[domains[1]] = w
+                #print csv_output[domains[1]]
+    except KeyboardInterrupt:
+        exit(0)
+    except:
+        pass
 
 '''# File Write for Output
 with open('domains_with_bah_email.csv', 'w') as csvfile:
